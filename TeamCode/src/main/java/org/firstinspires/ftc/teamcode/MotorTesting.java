c4015db8eb67f88570ed84775c564b67e1183830
Aiden <111412941+Aideniscool123@users.noreply.github.com>
2026-09-13 15:18:10 -0700

    3 motorsensor testing (#4)
    * Added motor testing code with variable speed wit triggers on controller.
    
    * Added servo testing, controlled with a controller
    
    * Added the Motor Testing made by Nicolas
    
    * Setup Motor Testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTesting.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+//Made by Nicolas
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+@TeleOp
+public class MotorTesting extends OpMode {
+    private DcMotorEx motor;
+    private TouchSensor touch;
+
+    @Override
+    public void init() {
+        motor = hardwareMap.get(DcMotorEx.class, "motorName");
+        touch = hardwareMap.get(TouchSensor.class, "sensorName");
+
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+        if(touch.isPressed()) {
+            motor.setPower(0);
+        } else {
+            motor.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
+        }
+    }
+}
