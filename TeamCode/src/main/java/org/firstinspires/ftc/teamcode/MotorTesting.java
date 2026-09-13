d43d5cd624a14275c0112a2e4332765037b3d06f
Aiden <aidenrafferty74@gmail.com>
2026-09-13 14:23:42 -0700

    Added the Motor Testing made by Nicolas

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTesting.java
@@ -1,28 +1,34 @@
 package org.firstinspires.ftc.teamcode;
+//Made by Nicolas
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
-@TeleOp(name = "Motor Testing", group = "TeleOp")
-public class MotorTesting extends LinearOpMode {
-
-    private DcMotor motor;
+@TeleOp
+public class MotorTesting extends OpMode {
+    private DcMotorEx motor;
+    private TouchSensor touch;
 
     @Override
-    public void runOpMode() {
-        motor = hardwareMap.get(DcMotor.class, "motor");
-        motor.setDirection(DcMotor.Direction.FORWARD);
+    public void init() {
+        motor = hardwareMap.get(DcMotorEx.class, "motorName");
+        touch = hardwareMap.get(TouchSensor.class, "sensorName");
+
         motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
 
-        waitForStart();
-
-        while (opModeIsActive()) {
-            double power = gamepad1.right_trigger - gamepad1.left_trigger;
-            motor.setPower(power);
-
-            telemetry.addData("Motor Power", power);
-            telemetry.update();
+    @Override
+    public void loop() {
+        if(touch.isPressed()) {
+            motor.setPower(0);
+        } else {
+            motor.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
         }
     }
 }
