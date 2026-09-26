1d7a7d70d85b742d2ec5ee3e7b18e13f4e996c5d
aiden <aidenrafferty74@gmail.com>
2026-09-25 19:53:07 -0700

    Added a basic test pose

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RedPreloadAllianceAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RedPreloadAllianceAuto.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
-
 import static com.pedropathing.api.Paths.line;
 
 import com.pedropathing.api.PoseFactory;
@@ -12,24 +11,17 @@ import com.seattlesolvers.solverslib.command.CommandScheduler;
 import com.seattlesolvers.solverslib.command.Commands;
 
 import org.firstinspires.ftc.teamcode.RobotContainer;
-import org.firstinspires.ftc.teamcode.chassis.ChassisCommands;
-
 
 @Autonomous(name = "RedAlliancePreload")
 public class RedPreloadAllianceAuto extends OpMode {
     private RobotContainer robot;
     private final PoseFactory p = PoseFactory.degrees();
 
-    private final Pose startPose = p.of(32.22493224932249, 14.902439024390237, 90);
-    private final Pose driveForwardPose = p.of(32.16176948564556, 41.512283907156146, 88);
-    private final Pose finalParkPose = p.of(30.674796747967477, 94.48915989159892, 88);
+    private final Pose startPose = p.of(0, 0, 90);
+    private final Pose driveForwardPose = p.of(0, 48, 90);
 
     private Path driveForwardPath() {
-        return line(startPose, driveForwardPose).linear(startPose, driveForwardPose);
-    }
-
-    private Path finalPath() {
-        return line(driveForwardPose, finalParkPose).linear(driveForwardPose, finalParkPose);
+        return line(startPose, driveForwardPose).tangent();
     }
 
     @Override
@@ -43,15 +35,13 @@ public class RedPreloadAllianceAuto extends OpMode {
     @Override
     public void start() {
         CommandScheduler.getInstance().schedule(Commands.sequence(
-            robot.followPath(driveForwardPath()),
-            Commands.waitMillis(1000),
-            robot.followPath(finalPath())
+                robot.followPath(driveForwardPath()),
+                Commands.waitMillis(1000)
         ));
     }
 
     @Override
     public void loop() {
         robot.periodic();
-
     }
 }
